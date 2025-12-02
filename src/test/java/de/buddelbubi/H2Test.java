142cba3e7363d3b039b7075303852c8d79158beb
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2025-12-02 11:47:15 +0100

    Update H2Test.java

--- src/test/java/de/buddelbubi/H2Test.java
+++ src/test/java/de/buddelbubi/H2Test.java
@@ -33,25 +33,7 @@ public class H2Test {
                 "    LastName varchar(255)," +
                 "    FirstName varchar(255)" +
                 ")");
-        client.executeUpdate("CREATE TABLE IF NOT EXISTS players (" +
-                "    userid VARCHAR(50)," +
-                "    lastname VARCHAR(50)," +
-                "    language VARCHAR(5)," +
-                "    customui TINYINT DEFAULT 1," +
-                "    prefix TINYINT DEFAULT 0," +
-                "    coins INT DEFAULT 0," +
-                "    gplaytime BIGINT DEFAULT 0," +
-                "    friendrequests VARCHAR(2000) DEFAULT '[]'," +
-                "    discordid VARCHAR(50)," +
-                "    season_xp INT DEFAULT 0," +
-                "    xp_global INT DEFAULT 0," +
-                "    gems INT DEFAULT 0," +
-                "    votes INT DEFAULT 0," +
-                "    login_streak INT DEFAULT 0," +
-                "    firstjoin TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
-                ") ENGINE=InnoDB;");
         client.executeUpdate("INSERT INTO Persons (PersonID, LastName, FirstName) VALUES (0, 'Bubi', 'Buddel')");
-        client.executeUpdate("INSERT INTO players (userid) VALUES ('test')");
     }
 
     @Test
@@ -60,10 +42,6 @@ public class H2Test {
         ResultSetList set = client.executeQuery("SELECT * FROM Persons WHERE PersonID = ?", 0);
         while (set.next()) {
             System.out.println(set.getString("LastName") + ", " + set.getString("FirstName"));
-        }
-        ResultSetList setList = client.executeQuery("SELECT * FROM players");
-        while (setList.next()) {
-            System.out.println(setList.getAvailableKeys());
         }
     }
 }
